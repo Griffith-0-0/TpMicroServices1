b4a6b615ce709d15dec17b5aa2956d004921cc99
Griffith-0-0 <ba_khafif@etu.enset-media.ac.ma>
2025-11-16 19:54:50 +0100

    commit discovery

---
+++ discorery-service/src/test/java/org/badr/discoreryservice/DiscoreryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.badr.discoreryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoreryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
